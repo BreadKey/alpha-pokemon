b52b8118d93f0b5cf660c03d99e6c777768b83ff
이영기 <t9rres@ajou.ac.kr>
2018-07-15 06:48:55 +0900

    add test case

--- common/tests.py
+++ common/tests.py
@@ -1,3 +1,54 @@
 from django.test import TestCase
+from pokedex.models import *
+from .templatetags import calculator
+from sample_maker.models import SamplePokemon
 
 # Create your tests here.
+class CalculatorTest(TestCase):
+
+    def setUp(self):
+        self.pokemon = make_pikachu()
+
+    def test_calculate_stab1(self):
+        move_type = PokemonType.objects.get(name__korean="전기")
+        stab = calculator.calculate_STAB(self.pokemon.spec, move_type)
+        expected_stab = 1.5
+
+        self.assertEqual(stab, expected_stab)
+
+    def test_calculate_stab2(self):
+        move_type = PokemonType.objects.create(name=Name.objects.create(korean="불꽃"))
+        stab = calculator.calculate_STAB(self.pokemon.spec, move_type)
+        expected_stab = 1
+
+        self.assertEqual(stab, expected_stab)
+
+def make_pikachu():
+    type = PokemonType.objects.get(name=Name.objects.get(korean="전기"))
+    name = Name.objects.create(korean="피카츄")
+    pikachu = PokemonSpec.objects.create(name=name, index_number=25)
+    pikachu.types.add(type)
+
+    return SamplePokemon.objects.create(spec=pikachu)
+
+def make_types():
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="노말"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="불꽃"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="물"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="전기"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="풀"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="얼음"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="격투"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="독"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="땅"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="비행"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="에스퍼"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="벌레"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="바위"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="고스트"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="드래곤"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="악"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="강철"))
+    PokemonType.objects.get_or_create(name=Name.objects.create(korean="페어리"))
+
+
--- sample_maker/tests.py
+++ sample_maker/tests.py
@@ -1,3 +1,50 @@
 from django.test import TestCase
+from .templatetags import manage_sample
+from .models import SamplePokemon
+from pokedex.models import *
+from common import constants
+from common.tests import make_pikachu, make_types
 
 # Create your tests here.
+
+class TestManageSample(TestCase):
+    def setUp(self):
+        make_types()
+        self.sample = make_pikachu()
+
+    def test_set_EV_bigger_than_max(self):
+        manage_sample.set_effort_value(self.sample, 'hp', 255)
+        hp_EV = self.sample.hp_EV
+        expected_hp_EV = constants.MAX_EV
+
+        self.assertEqual(hp_EV, expected_hp_EV)
+
+    def test_set_EV_smaller_than_min(self):
+        manage_sample.set_effort_value(self.sample, 'hp', -10)
+        hp_EV = self.sample.hp_EV
+        expected_hp_EV = constants.MIN_EV
+
+        self.assertEqual(hp_EV, expected_hp_EV)
+
+    def test_set_EV_not_integer(self):
+        manage_sample.set_effort_value(self.sample, 'hp', 'asd')
+        hp_EV = self.sample.hp_EV
+        expected_hp_EV = 0
+
+        self.assertEqual(hp_EV, expected_hp_EV)
+
+    def test_set_EV_over_max_sum(self):
+        manage_sample.set_effort_value(self.sample, 'hp', 252)
+        manage_sample.set_effort_value(self.sample, 'attack', 252)
+        manage_sample.set_effort_value(self.sample, 'defense', 252)
+
+        defense_EV = self.sample.defense_EV
+        expected_defense_EV = 6
+
+        self.assertEqual(defense_EV, expected_defense_EV)
+
+    def test_hidden_power1(self):
+        hidden_power_type = self.sample.hidden_power_type()
+        expected_HPT = PokemonType.objects.get(name__korean="악")
+
+        self.assertEqual(hidden_power_type, expected_HPT)
